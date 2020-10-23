f0a73b6f93155fb512fa93dded9202a5f96cf82c
cat-sun-1234 <45275829+cat-sun-1234@users.noreply.github.com>
2020-10-23 12:28:43 +1100

    level map finally done

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -5,8 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    private static GameStart _instance;
+
+    public static GameStart Instance { get { return _instance; } }
+
+
+    private void Awake() //singleton don't double up
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
     public AudioSource source;
     public AudioClip clip;
+    public int highScore;
+    public int prevTime; //this will be in seconds
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +43,17 @@ public class GameStart : MonoBehaviour
         source.Stop();
         source.clip = clip;
         source.Play();
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("GameScene");
 
     }
 
     public void ExitButton()
     {
         Application.Quit();
+    }
+
+    public void ExitLevel()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 }
